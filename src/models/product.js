ee18726b3695f86167d9a1f094a2ab4f132baffe
mafeHumnz <humanez036@gmail.com>
2026-04-10 12:58:34 -0500

    Creacion modelo product

---
+++ src/models/product.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+{
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  description: {
+    type: String
+  },
+
+  price: {
+    type: Number,
+    required: true
+  },
+
+  stock: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+
+  category: {
+    type: ObjectId,
+    ref: "Category"
+  },
+
+  image: {
+    type: String
+  },
+
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}
+,
+ {
+    timestamps: true,
+  }
+);
+
+export const Product = mongoose.model("Product", productSchema);
