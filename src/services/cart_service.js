22f5d308f20ce067ed1cefa434e27cd4e9e0dc4d
mafeHumnz <humanez036@gmail.com>
2026-05-01 21:58:18 -0500

    Creacion metodo addItem

--- src/services/cart_service.js
+++ src/services/cart_service.js
@@ -14,3 +14,34 @@ export const getCart = async (userId) => {
 
     return shoppingCart;
 };
+
+export const addItem = async (userId, productId, quantity) => {
+
+    let shoppingCart = await Cart.findOne({user:userId})
+
+    if(!shoppingCart) {
+        shoppingCart = await getCart(userId);
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+        throw new Error("Producto no existe");
+    }
+
+    const itemExistente = shoppingCart.items.find(item => item.product.toString() === productId);
+
+    if(itemExistente){
+        itemExistente.quantity += quantity;
+    }else{
+        shoppingCart.items.push({
+            product: productId, quantity
+        });
+    }
+
+    await shoppingCart.save();
+
+    const updateCart = await Cart.findOne({user:userId}).populate("items.product");
+
+    return updateCart;
+};
