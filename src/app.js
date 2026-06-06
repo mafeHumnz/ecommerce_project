933d086f44e6884d462418269b606fd04beff043
mafeHumnz <humanez036@gmail.com>
2026-06-05 20:05:43 -0500

    feat: implement authentication routing flow

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import productRoutes from "./routes/product_route.js";
+import authRoutes from "./routes/auth_route.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
