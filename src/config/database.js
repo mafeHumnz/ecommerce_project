abcc1932f6a1ff0e07566fabce15fce68e8df836
mafeHumnz <humanez036@gmail.com>
2026-06-14 15:33:54 -0500

    chore: configure mongodb atlas for production

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const conectarBD = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mi_base";
+    const uri = process.env.MONGO_URI || "mongodb+srv://ecommerce_admin:Chanchito@cluster0.ep5d74.mongodb.net/?appName=Cluster0";
 
     await mongoose.connect(uri);
     console.log("Conectado a MongoDB");
