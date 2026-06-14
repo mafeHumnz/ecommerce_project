8c67ff272110f4701cc47c8d71c62e2f7cf9aed9
mafeHumnz <humanez036@gmail.com>
2026-06-14 16:40:44 -0500

    successfull deployment

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const conectarBD = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb+srv://ecommerce_admin:Chanchito@cluster0.ep5d74.mongodb.net/?appName=Cluster0";
+    const uri = process.env.MONGO_URI;
 
     await mongoose.connect(uri);
     console.log("Conectado a MongoDB");
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { conectarBD } from "./config/database.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const startServer = async () => {
   try {
